d37a3bbf18b4a72e6562e177ab4c8c05d874afb0
cenc95 <919779778@qq.com>
2018-09-30 18:52:23 +0800

    test

---
+++ Spider/spider.py
@@ -0,0 +1,50 @@
+import requests
+import re
+import numpy as np
+
+def get_one_page(url):
+	response = requests.get(url)
+	if response.status_code == 200:
+		return response.text
+	return None
+
+def parse_one_page(html):
+	pattern = re.compile('<div class="price"><span>(.*?)</span>万</div>.*?(\d)室(\d)厅<span>/</span>(.*?)平米<span>', re.S)
+	items = re.findall(pattern, html)
+	return items
+
+def gradient_func(X, y, theta, m):
+	diff = np.dot(X, theta) - y
+	return (1. / m) * np.dot(np.transpose(X), diff)
+
+def gradient_descent(X, y, alpha, m):
+	theta = np.array([10, 1]).reshape(2, 1)
+	gradient = gradient_func(X, y, theta, m)
+	while not np.all(np.absolute(gradient) <= 1e-2):
+		theta = theta - alpha * gradient
+		gradient = gradient_func(X, y, theta, m)
+	return theta
+
+def main():
+	url = "https://bj.lianjia.com/ershoufang/pg1/"
+	html = get_one_page(url)
+	items = parse_one_page(html)
+
+	m = len(items)
+
+	X0 = np.arange(1, m+1).reshape(m, 1)
+	X1 = np.arange(1, m+1).reshape(m, 1)
+	y = np.arange(1, m+1).reshape(m, 1)
+
+	for i in range(len(items)):
+		y[i] = float(items[i][0])
+		X0[i] = float(items[i][3]) #price
+		X1[i] = float(items[i][1]) #bedroom
+	X = np.hstack((X0, X1))
+
+	print(gradient_descent(X, y, 0.0001, m))
+
+
+
+
+main()
---
+++ Spider/test.py
@@ -0,0 +1,31 @@
+import requests
+import re
+import numpy as np
+
+def get_one_page(url):
+	response = requests.get(url)
+	if response.status_code == 200:
+		return response.text
+	return None
+
+def parse_one_page(html):
+	pattern = re.compile('<div class="price"><span>(.*?)</span>万</div>.*?(\d)室(\d)厅<span>/</span>(.*?)平米<span>', re.S)
+	items = re.findall(pattern, html)
+	return items
+
+url = 'https://bj.lianjia.com/ershoufang/pg1/'
+html = get_one_page(url)
+print(parse_one_page(html))
+m = 10
+
+X0 = np.arange(1, m+1).reshape(m, 1)
+X1 = np.arange(1, m+1).reshape(m, 1)
+X2 = np.arange(1, m + 1).reshape(m, 1)
+y = np.arange(1, m+1).reshape(m, 1)
+X = np.hstack((X0, X1, X2))
+
+theta = np.array([1, 1, 1]).reshape(3, 1)
+
+diff = np.dot(X, theta)
+
+print(diff)
